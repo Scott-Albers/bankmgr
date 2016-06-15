43c87694bc835dc539e5b50b5c2e466d09b22b28
Michael Coonrod and Scott Albers <pair+michael+scott@allstate.com>
2016-06-15 17:00:53 -0500

    added basic test cases

---
+++ test/account.spec.js
@@ -0,0 +1,24 @@
+const expect = require('chai').expect;
+//require('../lib/account');
+
+describe('Account', function(){
+
+  describe('constructor', function(){
+    it('a new account should return name, balance, and create number', function(){
+      const account1 = new Account("Bob", 3.0);
+      expect(account1).to.be.instanceof(Account);
+      expect(account1.number).to.be.length(36);
+
+      expect(account1.number).not.to.be.undefined;
+      expect(account1.name).to.equal("Bob");
+      expect(account1.balance).to.equal(3.0);
+
+      expect(account1.deposits).to.be.length(0);
+      expect(account1.withdraws).to.be.length(0);
+
+
+    });
+
+    });
+
+  });
