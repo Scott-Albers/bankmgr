2e1091259a58d7551ae76fc262892b92b9d7af70
Michael Coonrod and Scott Albers <pair+michael+scott@allstate.com>
2016-06-16 10:37:42 -0500

    Clean-up for new style rules

--- test/account.spec.js
+++ test/account.spec.js
@@ -1,24 +1,19 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable prefer-arrow-callback */
 const expect = require('chai').expect;
 const Account = require('../lib/account');
 
-describe('Account', function(){
-
-  describe('constructor', function(){
-    it('a new account should return name, balance, and create number', function(){
-      const account1 = new Account("Bob", 3.0);
+describe('Account', function testAccount() {
+  describe('constructor', function testConstructor() {
+    it('a new account should return name, balance, and create number', function testCase1() {
+      const account1 = new Account('Bob', 3.0);
       expect(account1).to.be.instanceof(Account);
       expect(account1.number).to.be.length(36);
-
       expect(account1.number).not.to.be.undefined;
-      expect(account1.name).to.equal("Bob");
+      expect(account1.name).to.equal('Bob');
       expect(account1.balance).to.equal(3.0);
-
       expect(account1.deposits).to.be.length(0);
       expect(account1.withdraws).to.be.length(0);
-
-
     });
-
-    });
-
   });
+});
